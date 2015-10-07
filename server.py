111ca2f179a4d317faa728ad2d4236a835b70061
Jaak Erisalu <jaak.erisalu@gmail.com>
2015-10-07 19:36:53 +0300

    Divide tasks between slaves

--- server.py
+++ server.py
@@ -8,6 +8,7 @@ import sys
 from urllib.error import URLError
 from urllib.parse import urlparse, parse_qs
 import urllib.request as urlr
+import time
 
 cgitb.enable()
 
@@ -23,10 +24,12 @@ class MyHandler(BaseHTTPRequestHandler):
     def do_POST(s):
         s.send_response(200)
         s.end_headers()
-        length = int(s.headers['Content-Length'])
-        post_data = urllib.parse.parse_qs(s.rfile.read(length).decode('utf-8'))
 
-        print("POST YO", post_data)
+        parser = urlparse(s.path)
+        if parser.path == "/startcrack":
+            length = int(s.headers['Content-Length'])
+            post_data = urllib.parse.parse_qs(s.rfile.read(length).decode('utf-8'))
+            print("CHECK OUT MY CRACK YO", post_data)
 
     def do_GET(s):
         """Respond to a GET request."""
@@ -47,21 +50,22 @@ class MyHandler(BaseHTTPRequestHandler):
             qs = parse_qs(parser.query)
             print('MD5', qs['md5'])
             make_resource_request(noask=["http://127.0.0.1:%s" % CURRENT_SERVER_STATUS['port']], ttl=5, id='yolocrack')
-            CURRENT_SERVER_STATUS['waiting'] = True
-
-            md5 = qs['md5']
-            is_boss = True
+            # CURRENT_SERVER_STATUS['waiting'] = True
+            time.sleep(5)
+            # CURRENT_SERVER_STATUS['waiting'] = False
+            init_cracker(qs['md5'])
 
         if parser.path == "/resource":
             qs = parse_qs(parser.query)
             # The server gets a request from themselves once they finish sending out all other requests. Ignore it
+            print(parse_qs(parser.query))
             if int(qs['sendport'][0]) == CURRENT_SERVER_STATUS['port'] and socket.gethostbyname(socket.gethostname()) == qs['sendip'][0]:
                 return
 
-            print(parse_qs(parser.query))
             ttl = int(qs['ttl'][0]) - 1
             if ttl > 1:
-                make_resource_request(noask=["http://" + x.replace("_", ":") for x in qs['noask']], ttl=ttl, id=qs['id'][0])
+                make_resource_request(noask=["http://" + x.replace("_", ":") for x in qs['noask']], ttl=ttl,
+                                      id=qs['id'][0], ip=qs['sendip'][0], port=qs['sendport'][0])
 
             # MAKE RESPONSE
             if not CURRENT_SERVER_STATUS['waiting']:
@@ -75,7 +79,6 @@ class MyHandler(BaseHTTPRequestHandler):
                 'ip': "http://" + qs['sendip'][0] + ":" + qs['sendport'][0],
                 'resource_amount': qs['available'][0]
             })
-
 
             if len(CURRENT_SERVER_STATUS['slaves']) == 3:
                 print(CURRENT_SERVER_STATUS['slaves'])
@@ -104,7 +107,7 @@ def get_machines():
     return loc + res
 
 
-def make_resource_request(noask=None, ttl=5, id="gregorjaakrannar"):
+def make_resource_request(noask=None, ttl=5, id="gregorjaakrannar", ip=None, port=None):
     if not noask:
         noask = []
 
@@ -122,11 +125,12 @@ def make_resource_request(noask=None, ttl=5, id="gregorjaakrannar"):
     Final url template:
     http://11.22.33.44:2345/resource?sendip=55.66.77.88&sendport=6788&ttl=5&id=wqeqwe23&noask=11.22.33.44_345&noask=111.222.333.444_223
     """
-    params = "/resource?sendip=" + socket.gethostbyname(socket.gethostname())\
-             + "&sendport=" + str(CURRENT_SERVER_STATUS['port'])\
+    params = "/resource?sendip=" + (socket.gethostbyname(socket.gethostname()) if not ip else ip) \
+             + "&sendport=" + (str(CURRENT_SERVER_STATUS['port']) if not ip else port) \
              + "&ttl=" + str(ttl)\
              + "&id=" + str(id)\
              + "&noask=" + noask_list
+    print(params)
 
     for machine in machines:
         req = urlr.Request(machine + params)
@@ -155,31 +159,51 @@ def make_ready_response(target):
         print("Socket timed out on success")
 
 
-def post_test(md5):
-    print("POSTIN")
-    url="http://127.0.0.1:9002/"
-    values = {"ip": socket.gethostbyname(socket.gethostname()),
-              "port": CURRENT_SERVER_STATUS['port'],
-              "md5": md5,
-              "ranges": ["ax?o?ssss","aa","ab","ac","ad"],
-              "wildcard": "?",
-              "symbolrange": [[3,10],[100,150]]
-            }
+def make_assignment(ip, tasks, md5):
+    print("POSTIN CRACK ASSIGNMENT")
+    url = ip + "/startcrack"
+    values = {
+        "ip": socket.gethostbyname(socket.gethostname()),
+        "port": CURRENT_SERVER_STATUS['port'],
+        "md5": md5,
+        "ranges": tasks,
+        "wildcard": "?",
+    }
 
     data = urllib.parse.urlencode(values)
     binary_data = data.encode('utf-8')
-    req = urllib.request.Request(url, binary_data)
+    req = urlr.Request(url, binary_data)
     try:
         print("Req", req)
-        response = urllib.request.urlopen(req)
-        the_page = response.read()
+        response = urlr.urlopen(req)
     except urlr.http.client.HTTPException as e:
         print(e)
 
 
-def init_cracker():
-    make_resource_request()
+def init_cracker(md):
+    # make_resource_request()
     # peaks time outima, vastuseid ootama.
+    threes = ["?"]
+    fours = ["??"]
+    char = 32  # start with this char ascii (space)
+    while char < 126:
+        threes.append(str(chr(char)) + "??")
+        fours.append(str(chr(char)) + "???")
+        char += 1
+
+    task_count = len(threes) + len(fours)  # Spoiler: 190
+    slaves = len(CURRENT_SERVER_STATUS['slaves'])
+    each = int(task_count / slaves)
+    i = 0
+    for slave in CURRENT_SERVER_STATUS['slaves']:
+        i += 1
+        if i == slaves:
+            tasks = threes + fours
+        else:
+            tasks = threes[:int(each/2)] + fours[:int(each/2)]
+            threes = threes[int(each/2):]
+            fours = fours[int(each/2):]
+        make_assignment(slave['ip'], tasks, md)
 
 
 class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
