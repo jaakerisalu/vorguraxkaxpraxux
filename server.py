958fb5cd6d87ade74453529dd87929f77aec7294
Jaak Erisalu <jaak.erisalu@gmail.com>
2015-10-07 09:32:18 +0300

    Add multithreading and slave communication

--- server.py
+++ server.py
@@ -1,15 +1,17 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import cgitb;
 import json
+import random
 import socket
+from socketserver import ThreadingMixIn
 import sys
 from urllib.error import URLError
 from urllib.parse import urlparse, parse_qs
 
-cgitb.enable()  ## This line enables CGI error reporting
+cgitb.enable()
 import urllib.request as urlr
 
-CURRENT_SERVER_PORT = 0
+CURRENT_SERVER_STATUS = {'slaves':[]}
 
 class MyHandler(BaseHTTPRequestHandler):
     def do_HEAD(s):
@@ -23,24 +25,22 @@ class MyHandler(BaseHTTPRequestHandler):
         s.send_header("Content-type", "text/html")
         s.end_headers()
 
-        print('Sain k2tte yo')
+        print('GET')
 
         parser = urlparse(s.path)
-        # print(parser)
-
+        print(parser)
 
         if parser.path == "/crack":
             qs = parse_qs(parser.query)
 
             print('MD5', qs['md5'])
-            make_resource_request(noask=["http://http://127.0.0.1:9000/"], ttl=5, id='yolocrack')
-
-
+            make_resource_request(noask=["http://127.0.0.1:%s" % CURRENT_SERVER_STATUS['port']], ttl=5, id='yolocrack')
+            CURRENT_SERVER_STATUS['waiting'] = True
 
         if parser.path == "/resource":
             qs = parse_qs(parser.query)
             # The server gets a request from themselves once they finish sending out all other requests. Ignore it
-            if int(qs['sendport'][0]) == CURRENT_SERVER_PORT and socket.gethostbyname(socket.gethostname()) == qs['sendip'][0]:
+            if int(qs['sendport'][0]) == CURRENT_SERVER_STATUS['port'] and socket.gethostbyname(socket.gethostname()) == qs['sendip'][0]:
                 return
 
             print(parse_qs(parser.query))
@@ -49,8 +49,20 @@ class MyHandler(BaseHTTPRequestHandler):
                 make_resource_request(noask=["http://" + x.replace("_", ":") for x in qs['noask']], ttl=ttl, id=qs['id'][0])
 
             #MAKE RESPONSE
-            s.wfile.write(bytes("<html><head><title>you accessed path %s</title></head>" % s.path, 'UTF-8'))
+            if not CURRENT_SERVER_STATUS['waiting']:
+                print('I must respond')
+                make_ready_response(qs['sendip'][0] + ":" + qs['sendport'][0])
 
+        if parser.path == "/ready":
+            qs = parse_qs(parser.query)
+            print("I RECIEVED A RESPONCE FROM SLAVE")
+            CURRENT_SERVER_STATUS['slaves'].append({
+                'ip': "http://" + qs['sendip'][0] + ":" + qs['sendport'][0],
+                'resource_amount': qs['available'][0]
+            })
+
+            if len(CURRENT_SERVER_STATUS['slaves']) == 3:
+                print(CURRENT_SERVER_STATUS['slaves'])
 
 def get_machines():
     """
@@ -83,14 +95,14 @@ def make_resource_request(noask=None, ttl=5, id="gregorjaakrannar"):
     if len(noask):
         machines = [x for x in machines if x not in noask]
 
-    noask_list = "&noask=".join([x.replace("http://", "").replace(":", "_") for x in machines + noask])
+    noask_list = "&noask=".join([x.replace("http://", "").replace(":", "_") for x in list(set(machines + noask))])
 
     """
     Final url template:
     http://11.22.33.44:2345/resource?sendip=55.66.77.88&sendport=6788&ttl=5&id=wqeqwe23&noask=11.22.33.44_345&noask=111.222.333.444_223
     """
     params = "/resource?sendip=" + socket.gethostbyname(socket.gethostname())\
-             + "&sendport=" + str(CURRENT_SERVER_PORT)\
+             + "&sendport=" + str(CURRENT_SERVER_STATUS['port'])\
              + "&ttl=" + str(ttl)\
              + "&id=" + str(id)\
              + "&noask=" + noask_list
@@ -106,19 +118,40 @@ def make_resource_request(noask=None, ttl=5, id="gregorjaakrannar"):
         except socket.timeout as e:
             print("Socket timed out, moving on")
 
+
+def make_ready_response(target):
+    params = "/ready?sendip=" + socket.gethostbyname(socket.gethostname())\
+             + "&sendport=" + str(CURRENT_SERVER_STATUS['port'])\
+             + "&available=%d" % random.randint(0, 100)
+    req = urlr.Request("http://" + target + params)
+    print("Trying to request: " + "http://" + target + params)
+    try:
+        response = urlr.urlopen(req, timeout=1)
+        r = response.read()
+    except URLError as e:
+        print("Success response failed " + str(e.reason))
+    except socket.timeout as e:
+        print("Socket timed out on success")
+
 def init_cracker():
     make_resource_request()
     # peaks time outima, vastuseid ootama.
 
+class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
+    pass
+    # top le kek
+
 def run(port):
-    server = HTTPServer
+    server = ThreadedHTTPServer
     handler = MyHandler
     server_address = ("", port)
 
-    global CURRENT_SERVER_PORT
-    CURRENT_SERVER_PORT = port
+    global CURRENT_SERVER_STATUS
+    CURRENT_SERVER_STATUS['port'] = port
+    CURRENT_SERVER_STATUS['waiting'] = False
 
     httpd = server(server_address, handler)
+    print("I am waiting on port %s" % port)
 
     httpd.serve_forever()
 
@@ -127,4 +160,5 @@ if __name__ == '__main__':
         run(int(sys.argv[1]))
     else:
         print('Run with server.py my_port')
+
 
